0bf8ba00ab7667abc9e7b5121f7a4a1c2add7d20
Liz P <parisielizabeth@gmail.com>
2021-03-19 23:34:01 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,24 +1,24 @@
 // // write and export your action creator function here
 
-export const fetchCats = () => {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_CATS' })
-    
-    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
-        return response.json()
-    }).then(responseJSON => {
-        dispatch({ type: 'ADD_CATS', cats: response.JSON.images})
-    })
-  }
-}
-
 // export const fetchCats = () => {
 //   return (dispatch) => {
-//     dispatch({ type: 'LOADING_CATS'})
+//     dispatch({ type: 'LOADING_CATS' })
+    
 //     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
-//       return response.json()
+//         return response.json()
 //     }).then(responseJSON => {
-//       dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+//         dispatch({ type: 'ADD_CATS', cats: response.JSON.images})
 //     })
 //   }
 // }
+
+export const fetchCats = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_CATS'})
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+      })
+    }
+  }
