9294b5ad4df778949201c4cfb6c3b40a7e12bca1
Liz P <parisielizabeth@gmail.com>
2021-03-19 23:34:46 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -14,7 +14,7 @@
 
 export const fetchCats = () => {
     return (dispatch) => {
-      dispatch({ type: 'LOADING_CATS'})
+      dispatch({ type: 'LOADING_CATS' })
       fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
         return response.json()
       }).then(responseJSON => {
