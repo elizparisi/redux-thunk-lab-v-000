8a677b70daa7b651503175a8f2c59f880bd56c14
Liz P <parisielizabeth@gmail.com>
2021-03-19 23:36:45 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -7,18 +7,7 @@ export const fetchCats = () => {
     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
         return response.json()
     }).then(responseJSON => {
-        dispatch({ type: 'ADD_CATS', cats: response.JSON.images})
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images})
     })
   }
 }
-
-export const fetchCats = () => {
-    return (dispatch) => {
-      dispatch({ type: 'LOADING_CATS' })
-      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
-        return response.json()
-      }).then(responseJSON => {
-        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
-      })
-    }
-  }
