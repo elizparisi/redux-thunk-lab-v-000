377f02dd4ec66c68911e4ecd09237261d7ac5f8b
Liz P <parisielizabeth@gmail.com>
2021-03-19 23:35:39 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,16 +1,16 @@
 // // write and export your action creator function here
 
-// export const fetchCats = () => {
-//   return (dispatch) => {
-//     dispatch({ type: 'LOADING_CATS' })
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' })
     
-//     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
-//         return response.json()
-//     }).then(responseJSON => {
-//         dispatch({ type: 'ADD_CATS', cats: response.JSON.images})
-//     })
-//   }
-// }
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+        return response.json()
+    }).then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: response.JSON.images})
+    })
+  }
+}
 
 export const fetchCats = () => {
     return (dispatch) => {
